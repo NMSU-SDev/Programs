9c95f7deb1aa5d49e3c0d451e259a79ef5927c42
Brandon Z. Lujan <bzlujapv@nmsu.edu>
2020-08-25 22:19:44 -0600

    Adding to existing project a Questions directory and HelloWorld program

---
+++ HelloWorld/src/edu/nmsu/cs/helloworld/helloworld.java
@@ -0,0 +1,15 @@
+/* helloworld.java
+ * Program that prints out the message "Hello world!" in the console.
+ * Lab1
+ * @author Brandon Z. Lujan
+ */
+
+package edu.nmsu.cs.helloworld;
+
+public class helloworld {
+
+	public static void main(String[ ] args) {
+		System.out.println("Hello world!");
+	} //end main
+
+} //end class
