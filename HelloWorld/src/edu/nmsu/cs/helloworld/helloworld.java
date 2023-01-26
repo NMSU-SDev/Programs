fb35ba7c731af5fcd49573c7e967ff9badc6e244
Jared K <jarod137@nmsu.edu>
2023-01-26 15:22:43 -0700

    implemented hello world

---
+++ HelloWorld/src/edu/nmsu/cs/helloworld/helloworld.java
@@ -0,0 +1,10 @@
+package edu.nmsu.cs.helloworld;
+
+//Jared Kaiser CS 370
+//Hello world program to familiarize myself with git
+
+public class helloworld {
+    public static void main(String[] args) {
+        System.out.print("Hello World");
+    }
+}
