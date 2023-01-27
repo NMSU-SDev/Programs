78b873b752db1b83807d6f19301c9be440b0a187
Specioushawks8 <mathnerd@nmsu.edu>
2023-01-26 22:58:16 -0700

    Added new Helloworld file

---
+++ HelloWorld/src/edu/nmsu/cs/helloworld/helloworld.java
@@ -0,0 +1,16 @@
+package edu.nmsu.cs.helloworld;
+
+
+
+// This is a test :D
+public class helloworld {
+
+	public static void main( String args [] )  {
+
+		System.out.println( "hello world! " ); 
+
+	}// end main
+
+
+
+}// end class
