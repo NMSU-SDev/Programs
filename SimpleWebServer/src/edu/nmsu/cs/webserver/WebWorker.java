77591d21360cf6476c7a01ba3206a0a0fb1a5c19
Trang Tran <111462670+trangtran321@users.noreply.github.com>
2023-03-20 09:06:27 -0600

    WebWorker can determine what type of file
    Program can serve text and image files

--- SimpleWebServer/src/edu/nmsu/cs/webserver/WebWorker.java
+++ SimpleWebServer/src/edu/nmsu/cs/webserver/WebWorker.java
@@ -21,6 +21,7 @@ package edu.nmsu.cs.webserver;
 
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
@@ -58,10 +59,15 @@ public class WebWorker implements Runnable
 		{
 			InputStream is = socket.getInputStream();
 			OutputStream os = socket.getOutputStream();
+
+			//Read in request and return a file name
 			String requests = readHTTPRequest(is);
-			File fil = writeHTTPHeader(os, "text/html", requests);
-			writeContent(os, fil);
-			
+
+			//Finds the content type of a request
+			String contentType = getContentType(requests);
+			File fil = writeHTTPHeader(os, contentType, requests);
+			writeContent(os, contentType, fil);
+
 			os.flush();
 			socket.close();
 		}
@@ -78,8 +84,8 @@ public class WebWorker implements Runnable
 	 **/
 	private String readHTTPRequest(InputStream is)
 	{
-		String line; 
-		String [] requests = new String[25]; 
+		String line;
+		String [] requests = new String[25];
 		int count = 0;
 		BufferedReader r = new BufferedReader(new InputStreamReader(is));
 		while (true)
@@ -90,15 +96,19 @@ public class WebWorker implements Runnable
 					Thread.sleep(1);
 				line = r.readLine();
 				System.err.println("Request line: (" + line + ")");
-				
-				//Get the request string after http verb 
-				requests[count] = line.split(" ")[1]; 
-				count++; 
-				
+
+				//As long as the request is not an empty line, split the line and retain
+				//the actual request from server in requests array
+				if (line.length() > 0){
+				//Get the request string after http verb
+				requests[count] = line.split(" ")[1];
+				count++;
+				}
+
 				if (line.length() == 0){
 					break;
 				}
-				
+
 			}
 			catch (Exception e)
 			{
@@ -107,85 +117,117 @@ public class WebWorker implements Runnable
 			}
 		}
 
-		String addressName = requests[0]; 
-		return addressName;
+		//return the server's requested file name
+		return requests[0];
 	}
 
 	/**
 	 * Write the HTTP header lines to the client network connection.
-	 * 
+	 *
 	 * @param os
 	 *          is the OutputStream object to write to
 	 * @param contentType
 	 *          is the string MIME content type (e.g. "text/html")
+	 * @return File req
+	 * 			Requested file from server
 	 **/
 	private File writeHTTPHeader(OutputStream os, String contentType, String requests) throws Exception
 	{
 		Date d = new Date();
 		DateFormat df = DateFormat.getDateTimeInstance();
 		df.setTimeZone(TimeZone.getTimeZone("GMT"));
-		String path = "./src/edu/nmsu/cs/webserver" + requests;
-		System.out.println("THIS IS THE PATH:: " + path + "html") ;
-		
-		File req = new File(path); 
-		
-		if (req.exists()){
+
+		//Build the path for a requested file and create a File object based on path
+		String path = "./www/res/acc" + requests;
+		File req = new File(path);
+
+
+		//if the file exists or is favicon.ico, print 200 OK, else print out 404 Not Found
+		if(requests.contains("favicon")){
 			os.write("HTTP/1.1 200 OK\n".getBytes());}
-		else{
+		else if (req.exists()){
+			os.write("HTTP/1.1 200 OK\n".getBytes());}
+		else if (!req.exists()){
 			os.write("HTTP/1.1 404 NOT FOUND\n".getBytes());}
 
 		os.write("Date: ".getBytes());
 		os.write((df.format(d)).getBytes());
 		os.write("\n".getBytes());
-		os.write("Server: localhost\n".getBytes());
-		// os.write("Last-Modified: Wed, 08 Jan 2003 23:11:55 GMT\n".getBytes());
-		// os.write("Content-Length: 438\n".getBytes());
+		os.write("Server: Trang\n".getBytes());
 		os.write("Connection: close\n".getBytes());
 		os.write("Content-Type: ".getBytes());
 		os.write(contentType.getBytes());
 		os.write("\n\n".getBytes()); // HTTP header ends with 2 newlines
+
 		return req;
 	}
 
 	/**
 	 * Write the data content to the client network connection. This MUST be done after the HTTP
 	 * header has been written out.
-	 * 
+	 *
 	 * @param os
 	 *          is the OutputStream object to write to
 	 * I used an article in GeeksForGeeks to help me write the code to serve a file.
 	 * https://www.geeksforgeeks.org/java-program-to-extract-content-from-a-html-document/
 	 * @throws IOException
-	 * 
+	 *
 	 **/
-	private void writeContent(OutputStream os, File fil) throws IOException
-	{	
-		
-		StringBuilder pageCode = new StringBuilder(); 
-		
+	private void writeContent(OutputStream os, String contentType, File fil) throws IOException
+	{
 		/**
-		* if the file exists, pageCode - the StringBuilder object - will append every line 
-		* of code from the html file into itself using s - the BufferedReader object. 
-		* The result is then written into the output stream and served. 
+		* if file exists, depending on the contentType, write to output stream the appropriate
+		* bytes or text
 		*/
 		if (fil.exists() && !fil.isDirectory()) {
-			 // write file contents 
-			FileReader fr = new FileReader(fil); 
-			BufferedReader br = new BufferedReader(fr);
-			String s; 
-			while ((s = br.readLine()) != null) {
-				pageCode.append(s); 
+			 //if file request is for favicon-icon or other image file, the bytes will be written
+			 //to the output stream
+			 if (contentType.contains("image")){
+				FileInputStream fis = new FileInputStream(fil);
+				int cursor;
+
+				while((cursor = fis.read()) != -1) {
+					os.write(cursor);
+				}
+
+				fis.close();
 			}
-			String htmlLines = pageCode.toString(); 
-				
-			os.write("<html><head></head><body>\n".getBytes());
-			os.write(htmlLines.getBytes());
-			os.write("</body></html>\n".getBytes());
-			br.close(); 
-			fr.close(); 
-				
+
+			//if it is an html file, bufferedReader will pull every html line & append it
+			//to one String. This string will be what is outputed to server
+			else if(contentType.contains("html")){
+				FileReader fr = new FileReader(fil);
+				BufferedReader br = new BufferedReader(fr);
+				String s;
+				String htmlLines = "";
+				while ((s = br.readLine()) != null) {
+					htmlLines += s;
+				}
+
+				//find and output <img> tag from html
+				htmlLines = htmlLines.replaceAll("\"img\"", "lemon.jpg");
+
+				//set date
+				Date d = new Date();
+				DateFormat df = DateFormat.getDateTimeInstance();
+				df.setTimeZone(TimeZone.getTimeZone("MST"));
+				htmlLines = htmlLines.replaceAll("<today>", df.format(d));
+
+				//set server
+				htmlLines = htmlLines.replaceAll("<csServer>", "all about citrus");
+
+				//serve file
+				os.write("<html><head></head><body>\n".getBytes());
+				os.write(htmlLines.getBytes());
+				os.write("</body></html>\n".getBytes());
+				br.close();
+				fr.close();
+			}
 		}
 
+		/**
+		 * If there is no specified file name to output, but the directory exists
+		 */
 		else if (fil.exists() && fil.isDirectory()){
 			os.write("<html><head></head><body>\n".getBytes());
 			os.write("<h3>Homepage</h3>\n".getBytes());
@@ -200,7 +242,30 @@ public class WebWorker implements Runnable
 			os.write("<h3>404 Not Found</h3>\n".getBytes());
 			os.write("</body></html>\n".getBytes());
 		}
-	
+	}
+
+	/**
+	 * This method will find the content type requested
+	 * @param requests
+	 * @return String contentType
+	 */
+	private String getContentType(String requests){
+
+		if (requests.contains("jpg")){
+			return "image/jpeg";
+		}
+		else if (requests.contains("png")){
+			return "image/png";
+		}
+		else if (requests.contains("gif")){
+			return "image/gif";
+		}
+		else if (requests.contains("favicon")){
+			return "image/x-icon";
+		}
+		else {
+			return "text/html";
+		}
 	}
 
 } // end class
