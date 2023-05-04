f9c83d4d6779783317ed25ae642fb7091cf5a191
rhollomon <rubyh2@nmsu.edu>
2023-05-03 18:36:03 -0600

    Updated for P3

--- Coverage/src/edu/nmsu/cs/scoring/RacingScore2.java
+++ Coverage/src/edu/nmsu/cs/scoring/RacingScore2.java
@@ -36,25 +36,20 @@ public class RacingScore2
 	public int overallScore()
 	{
 		int s, s1, s2;
-		if (score1 < score2 && score1 < score3)
+		if (score1 <= score2 && score1 <= score3) // if score1 is lowest
 		{
 			s1 = score2;
 			s2 = score3;
 		}
-		else if (score2 < score1 && score2 < score3)
+		else if (score2 <= score1 && score2 <= score3) // if score2 is lowest
+		{
+			s1 = score1;
+			s2 = score3;
+		}
+		else // if score3 is lowest
 		{
 			s1 = score1;
 			s2 = score2;
-		}
-		else if (score3 < score1 && score3 < score2)
-		{
-			s1 = score1;
-			s2 = score2;
-		}
-		else
-		{
-			s1 = 99;
-			s2 = 99;
 		}
 		s = s1 + s2;
 		return s;
@@ -63,7 +58,7 @@ public class RacingScore2
 	public static void main(String args[])
 	{
 		int s1, s2, s3;
-		if (args == null || args.length != 3)
+		if (args.length != 3)
 		{
 			System.err.println("Error: must supply three arguments!");
 			return;
