bed1f0ad48aa977bc5394ae82c9375078325b570
MoriMnz <munozmau88@gmail.com>
2023-05-11 18:46:35 -0600

    P3

--- Coverage/src/edu/nmsu/cs/scoring/RacingScore1.java
+++ Coverage/src/edu/nmsu/cs/scoring/RacingScore1.java
@@ -3,38 +3,37 @@ package edu.nmsu.cs.scoring;
 /***
  * Olympic Dragon Racing Scoring Class
  *
- * For the Summer Olympics dragon racing event there are three judges, each of which gives a score
- * from 0 to 50 (inclusive), but the lowest score is thrown out and the competitor's overall score
- * is just the sum of the two highest scores. This class supports the recording of the three judge's
+ * For the Summer Olympics dragon racing event there are three judges, each of
+ * which gives a score
+ * from 0 to 50 (inclusive), but the lowest score is thrown out and the
+ * competitor's overall score
+ * is just the sum of the two highest scores. This class supports the recording
+ * of the three judge's
  * scores, and the computing of the competitor's overall score.
  *
  ***/
 
-public class RacingScore1
-{
+public class RacingScore1 {
 
-	int	score1;
+	int score1;
 
-	int	score2;
+	int score2;
 
-	int	score3;
+	int score3;
 
-	public RacingScore1()
-	{
+	public RacingScore1() {
 		score1 = 0;
 		score2 = 0;
 		score3 = 0;
 	}
 
-	public void recordScores(int s1, int s2, int s3)
-	{
+	public void recordScores(int s1, int s2, int s3) {
 		score1 = s1;
 		score2 = s2;
 		score3 = s3;
 	}
 
-	public int overallScore()
-	{
+	public int overallScore() {
 		int s;
 		if (score1 < score2)
 			s = score2;
@@ -46,22 +45,17 @@ public class RacingScore1
 		return s;
 	}
 
-	public static void main(String args[])
-	{
+	public static void main(String args[]) {
 		int s1, s2, s3;
-		if (args.length != 3)
-		{
+		if (args.length != 3) {
 			System.err.println("Error: must supply three arguments!");
 			return;
 		}
-		try
-		{
+		try {
 			s1 = Integer.parseInt(args[0]);
 			s2 = Integer.parseInt(args[1]);
 			s3 = Integer.parseInt(args[2]);
-		}
-		catch (Exception e)
-		{
+		} catch (Exception e) {
 			System.err.println("Error: arguments must be integers!");
 			return;
 		}
